dd8215038f7a135a13d348484050ce42cbe572ce
dmitriy <ovchinnikovdk@yandex.ru>
2023-07-05 20:56:22 +0700

    feat: обернул проект в Docker, добавил Postgresql

--- app/config/database.py
+++ app/config/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL_SQLITE = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL_POSTGRESQL = "postgresql://postgres:postgres@db:5432/"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},  # "check_same_thread" only for SQLite
+    SQLALCHEMY_DATABASE_URL_POSTGRESQL,
+    # connect_args={"check_same_thread": False},  # "check_same_thread" only for SQLite
     echo=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
